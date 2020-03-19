05ca65a6e0391f3e7f2f236bd9bad035ec2ca67d
Fabricio Voznika <fvoznika@google.com>
2020-03-18 17:57:54 -0700

    Misc fixes to make stat_test pass (almost)
    The only test failing now requires socket which is not
    available in VFS2 yet.
    
    Updates #1198
    
    PiperOrigin-RevId: 301708149

--- pkg/bits/bits_template.go
+++ pkg/bits/bits_template.go
@@ -42,3 +42,8 @@ func Mask(is ...int) T {
 func MaskOf(i int) T {
 	return T(1) << T(i)
 }
+
+// IsPowerOfTwo returns true if 'v' is power of 2.
+func IsPowerOfTwo(v T) bool {
+	return v&(v-1) == 0
+}
